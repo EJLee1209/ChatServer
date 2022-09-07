8785195bb985bf53a25119ae750a47efe0ca91fe
EunJae <dldmswo1209@naver.com>
2022-09-07 17:18:44 +0900

    hello world

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/', require('./routes/rides'));
 
-
+app.get('/', (req,res)=>{
+    res.send('hello world');
+})
 
 server.listen(3000, function(){
     console.log("server on 8080")
